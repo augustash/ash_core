199115ee4b03c08e635e7fb83e4b97bf4b2100f6
Pete McWilliams <pmcwilliams@augustash.com>
2012-08-29 17:16:05 -0500

    Adds empty set of core extension files

---
+++ code/Helper/Data.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Ash Common Extension
+ *
+ * Maintains common settings and configuration for AAI built Magento websites.
+ *
+ * @category    Ash
+ * @package     Ash_Core
+ * @copyright   Copyright (c) 2012 August Ash, Inc. (http://www.augustash.com)
+ */
+
+/**
+ * Core data helper
+ *
+ * @category    Ash
+ * @package     Ash_Core
+ * @author      August Ash Team <core@augustash.com>
+ */
+class Ash_Core_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
